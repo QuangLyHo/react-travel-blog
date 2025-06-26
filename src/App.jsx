8d13b7d60da4933e49243880e1ab9823fb3b0b4c
Quang <76503650+QuangLyHo@users.noreply.github.com>
2025-06-26 16:08:59 -0400

    Updated app to use props

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import './App.css'
 import Header from './components/Header'
 import Entry from './components/Entry'
+import places from './data'
+
 
 function App() {
+
+  const placeComponents = places.map(place => (
+    <Entry 
+      key={place.id} 
+      title={place.title} 
+      text={place.text}  
+      dates={place.dates}
+      mapLink={place.googleMapsLink}
+      country={place.country}
+      img={place.img}
+      />
+  ))
+
   return (
     <>
       <Header />
-      <Entry />
+      <div className='card-container'>
+        {placeComponents}
+      </div>
     </>
   )
 }
--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,28 +1,19 @@
-import places from '../data.js'
 import locationMarker from '../images/marker.png'
 
-export default function Entry() {    
-    const data = places.map(place => {
-        return (
-            <article key={place.id} className='article-card'>
-                <div className='main-img-container'>
-                    <img className='main-img' src={place.img.src} alt={place.img.alt} />
-                </div>
-                <div className='card-details'>
-                    <img className='marker' src={locationMarker} alt="map marker icon" />
-                    <span className='country'>{place.country}</span>
-                    <a href={place.googleMapsLink}>View on Google Maps</a>
-                    <h2 className='details-title'>{place.title}</h2>
-                    <p className='details-date'>{place.dates}</p>
-                    <p className='details-text'>{place.text}</p>
-                </div>
-            </article>
-        )
-    })
-
+export default function Entry({title, text, dates, mapLink, country, img }) {  
     return (
-        <div className='card-container'>
-            {data}
-        </div>
+        <article className='article-card'>
+            <div className='main-img-container'>
+                <img className='main-img' src={img.src} alt={img.alt} />
+            </div>
+            <div className='card-details'>
+                <img className='marker' src={locationMarker} alt="map marker icon" />
+                <span className='country'>{country}</span>
+                <a href={mapLink}>View on Google Maps</a>
+                <h2 className='details-title'>{title}</h2>
+                <p className='details-date'>{dates}</p>
+                <p className='details-text'>{text}</p>
+            </div>
+        </article>
     )
 }
